20dac13defaa97db21ea651274a4e0e87325f66c
Jason Leatiota <jasole2007@yahoo.com>
2021-10-21 17:46:34 -0700

    player_score_routes

---
+++ app/api/player_scores_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify, request
+from app.models import PlayerScore, Player, db
+
+pscore_routes = Blueprint('player_scores', __name__)
+
+@pscore_routes.route('/')
+def player_scores():
+    player_scores = PlayerScore.query.all()
+    return {'playerScores': [{player_score.player_id: player_score.to_dict} for player_score in player_scores]}
+
+@pscore_routes.route('/update/<str:week>', methods=['PUT'])
+def update_player_scores(week):
+    
+    pscores = PlayerScore.query.filter_by(week='week').all()
+    for key, value in request.form:
+        for pscore in pscores:
+            setattr(pscore, key, value)
+    db.session.commit()
+    return {'message': 'updated'}
+
+
+        
+
+
+    
+    
