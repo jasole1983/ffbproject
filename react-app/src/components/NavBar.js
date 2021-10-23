390b414ede8be13557db7a7ae72438a84cecd2e0
Jason Leatiota <jasole2007@yahoo.com>
2021-10-23 04:53:48 -0700

    switching to FE

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,12 +1,20 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
-import {} from 'b'
+// import { NavLink } from 'react-router-dom';
+// import LogoutButton from './auth/LogoutButton';
+// import { NavBar } from 'bootstrap'
+import miniLogo from "../images/mywildestfantasylogoRough46.jpg"
 
 const NavBar = () => {
   return (
-    
+    <nav class="navbar nabar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src={miniLogo} class="d-inline-block align-top" alt="logo mwf"/>
+        My Wildest Fantasy
+      </a>
+      <span class="navbar gap">--</span>
+
+    </nav>
   );
 }
 
