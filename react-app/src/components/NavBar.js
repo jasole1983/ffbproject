9f63bab3f0af65f53075a108d4ce5d5abc486836
Jason Leatiota <jasole2007@yahoo.com>
2021-10-23 10:54:55 -0700

    NavBar Set

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,52 +2,61 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 // import LogoutButton from './auth/LogoutButton';
-// import { NavBar } from 'bootstrap'
+import Button from 'react-bootstrap/Button'
 import miniLogo from "../images/mywildestfantasylogoRough46.jpg"
 
 const NavBar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand font-weight-bolder" href="#">
         <img src={miniLogo} class="d-inline-block align-top" alt="logo mwf"/>
         My Wildest Fantasy
       </a>
       {/* <span class="navbar gap">--</span> */}
-      <div class="collapse navbar-collapse d-inline-block align-top" id="navbarNav">
+      <div class="collapse navbar-collapse d-inline-block align-top navbar-button-group" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
             <a class="nav-link" href="#">
-              <button class="btn bg-primary " type="button">
+              <Button class="btn bg-primary font-weight-bolder btn-lg " type="button">
                 League Home 
-              </button>    
+              </Button>    
             </a>
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="#">
-              <button class="btn bg-primary " type="button">
+              <Button class="btn bg-primary font-weight-bolder btn-lg " type="button">
                 Message Board 
-              </button>    
+              </Button>    
             </a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-aspopup="true" aria-expanded="false">
-              <button class="btn bg-primary " type="button">
-                My Team 
-              </button>    
+              <Button class="btn bg-primary font-weight-bolder btn-lg " type="button">
+                Manage Roster 
+              </Button>    
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               <a class="dropdown-item" href="#">View Roster</a>
               <a class="dropdown-item" href="#">Set Lineup</a>
               <a class="dropdown-item" href="#">Add/Drop</a>
               <a class="dropdown-item" href="#">Injured Reserve</a>
+              <a class="dropdown-item" href="#">Current Matchup</a>
             </div>
           </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
-              <button class="btn bg-primary " type="button">
-                
-              </button>    
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLinkTwo" role="button" data-toggle="dropdown" aria-aspopup="true" aria-expanded="false">
+              <Button class="btn bg-primary font-weight-bolder btn-lg" type="button">
+                League Actions
+              </Button>    
             </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkTwo">
+              <a class="dropdown-item" href="#">View All Rosters</a>
+              <a class="dropdown-item" href="#">Trade Center</a>
+              <a class="dropdown-item" href="#">Free Agents</a>
+              <a class="dropdown-item" href="#">League Standings</a>
+              <a class="dropdown-item" href="#">Weekly Results</a>
+              <a class="dropdown-item" href="#">Playoff Preview</a>
+            </div>
           </li>
         </ul>
       </div>
